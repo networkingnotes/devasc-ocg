c06fbe7fa07a429c90d96b884dae69497269b041
Bill <bill@burnam.net>
2020-11-18 00:38:29 -0600

    first file

---
+++ ch04-01.py
@@ -0,0 +1,8 @@
+class Router:
+    '''Router Class'''
+    def _init_(self, model, swversion, ip_add):
+        '''initialize values'''
+        self.model = model
+        self.swversion = swversion
+        self.ip_add = ip_add
+        
