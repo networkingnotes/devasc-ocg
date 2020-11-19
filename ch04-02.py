20f98e359a4d0816c4bbe40585ed9639545ac896
Bill Burnam <bill@burnam.net>
2020-11-18 20:40:52 -0600

    example 2 added

--- ch04-02.py
+++ ch04-02.py
@@ -13,8 +13,18 @@ class Router:
             f'Router Management Address :{self.ip_add}'
         return desc
 
+class Switch(Router):
+    def getdesc(self):
+        '''return a formatted description of the switch'''
+        desc = (f'Switch Model              :{self.model}\n'
+            f'Software Version          :{self.swversion}\n'
+            f'Switch Management Address :{self.ip_add}')
+        return desc
+
 rtr1 = Router('iosV', '15.6', '10.10.10.1')
 rtr2 = Router('isr', '16.9', '10.11.11.1')
+sw1 = Switch('Cat9300', '16.9.5', '10.10.10.8')
 
 print('Rtr1\n', rtr1.getdesc(), '\n', sep='')
 print('Rtr2\n', rtr2.getdesc(), sep='')
+print('Sw1\n', sw1.getdesc(), '\n', sep='')
