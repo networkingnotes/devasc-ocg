b2182a2bfa9bccb41e0f1d0ca102bc5fd29d0997
Bill Burnam <bill@burnam.net>
2020-11-19 23:15:44 -0600

    json example added

---
+++ ch05-04.py
@@ -0,0 +1,12 @@
+import csv
+
+print("Please add a new router to the list")
+hostname = input("What is the hostname? ")
+ip = input("What is the IP address? ")
+location = input("What is the location? ")
+
+router = [\" hostname \", \" ip \", \" location \"]
+
+with open("routerlist.csv", "a") as data:
+    csv_writer = csv.writer(data)
+    csv_writer.writerow(router)
