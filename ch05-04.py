a28f198d4a1e61a0a47975e379e4f0a079727e17
Bill Burnam <bill@burnam.net>
2020-11-25 12:33:04 -0600

    added QUOTE_ALL for CSV writer

--- ch05-04.py
+++ ch05-04.py
@@ -5,8 +5,8 @@ hostname = input("What is the hostname? ")
 ip = input("What is the IP address? ")
 location = input("What is the location? ")
 
-router = [\" hostname \", \" ip \", \" location \"]
+router = [hostname, ip, location]
 
 with open("routerlist.csv", "a") as data:
-    csv_writer = csv.writer(data)
+    csv_writer = csv.writer(data, quoting=csv.QUOTE_ALL)
     csv_writer.writerow(router)
